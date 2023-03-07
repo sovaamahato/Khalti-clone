c3e8f81dd28fe9f07f0fb1db2c8a7428870b9a0d
sova <mahatoshobha143@gmail.com>
2023-03-07 21:34:21 +0545

    support tab complete

--- lib/Pages/support.dart
+++ lib/Pages/support.dart
@@ -23,34 +23,102 @@ class Support extends StatelessWidget {
             )),
         elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Help & Support",
-              style: TextStyle(
-                  color: Colors.purple,
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10,),
-            Text(
-              " Customer Support, Feedbacks,FAQs",
-              style: TextStyle(
-                color: Colors.grey[500],
+      body: Container(
+        child: Padding(
+          padding: const EdgeInsets.only(top: 10.0, bottom: 10, left: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Help & Support",
+                style: TextStyle(
+                    color: Colors.purple,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold),
               ),
-            ),
-            //links---------------
-            TileForSupportTab(hinttext: "", name: "Facebook Messenger", icon: "lib/images/messenger.png",),
-            TileForSupportTab(hinttext: "", name: "Facebook Messenger", icon: "lib/images/whatsapp.png",),
-            TileForSupportTab(hinttext: "", name: "Facebook Messenger", icon: "lib/images/whatsapp.png",),
-            TileForSupportTab(hinttext: "", name: "Facebook Messenger", icon: "lib/images/whatsapp.png",),
-            TileForSupportTab(hinttext: "", name: "Facebook Messenger", icon: "lib/images/whatsapp.png",),
-            TileForSupportTab(hinttext: "", name: "Facebook Messenger", icon: "lib/images/whatsapp.png",),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                " Customer Support, Feedbacks,FAQs",
+                style: TextStyle(
+                  color: Colors.grey[500],
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              //links---------------
+              TileForSupportTab(
+                hinttext: "",
+                name: "Facebook Messenger",
+                icon: "lib/images/messenger.png",
+              ),
+              TileForSupportTab(
+                hinttext: "(WhatsApp - Chat Only)",
+                name: "9801165",
+                icon: "lib/images/whatsapp.png",
+              ),
+              TileForSupportTab(
+                hinttext: "(Viber - Chat Only)",
+                name: "khalti.com/viber",
+                icon: "lib/images/viber.png",
+              ),
+              TileForSupportTab(
+                hinttext: "(Toll Free)",
+                name: "011111111",
+                icon: "lib/images/old-phone.png",
+              ),
+              TileForSupportTab(
+                hinttext: "Landline",
+                name: "09292",
+                icon: "lib/images/old-phone.png",
+              ),
+              TileForSupportTab(
+                hinttext: "(Email Address)",
+                name: "support@khalti.com",
+                icon: "lib/images/old-phone.png",
+              ),
+              TileForSupportTab(
+                hinttext: "(WhatsApp - Chat Only)",
+                name: "Khalti Bluebook Service",
+                icon: "lib/images/whatsapp.png",
+              ),
+              //line
+              SizedBox(
+                height: 15,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 10.0),
+                child: Divider(
+                  thickness: 0.5,
+                  color: Colors.grey[500],
+                ),
+              ),
 
-          ],
+              SizedBox(
+                height: 15,
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.chat_bubble_outline,
+                  color: Colors.purple,
+                ),
+                title: Text("FAQs"),
+                subtitle: Text("Get help from FAQs"),
+                trailing: Icon(Icons.arrow_forward_ios_sharp,size: 20,),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.chat_bubble_outline,
+                  color: Colors.purple,
+                ),
+                title: Text("Report Issues"),
+                subtitle: Text("We would love to hear from you"),
+                trailing: Icon(Icons.arrow_forward_ios_sharp,size: 20,),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/components/tiles_for_support_tab.dart
+++ lib/components/tiles_for_support_tab.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 
 class TileForSupportTab extends StatelessWidget {
-  
   final name;
   final hinttext;
-  final icon;
-  TileForSupportTab({this.hinttext,required this.icon,required this.name});
-
+  final String icon;
+  TileForSupportTab({this.hinttext, required this.icon, required this.name});
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Row(children: [
-      Container(height: 30,width: 30,child: Image.asset(icon,fit: BoxFit.cover,),),
-      SizedBox(width: 10,),
-      Text(name),
-      SizedBox(width: 10,),
-      Text(hinttext,style: TextStyle(color: Colors.grey[600]),),
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Container(
+        child: Row(children: [
+          Container(
+            height: 20,
+            width: 20,
+            child: Image.asset(
+              icon,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Text(
+            name,
+            style: TextStyle(fontSize: 15),
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Expanded(
+            child: Text(
+              hinttext,
+              style: TextStyle(color: Colors.grey[600],fontSize: 13),
+            overflow: TextOverflow.ellipsis,
+            ),
 
-    ]),);
+          ),
+        ]),
+      ),
+    );
   }
 }
