32e7201b184d6e3c371399e3f0b7c9e25745de75
sova <mahatoshobha143@gmail.com>
2023-05-17 13:03:44 +0545

    scrolling functionality added

--- lib/components/automzatic_scroll_card.dart
+++ lib/components/automzatic_scroll_card.dart
@@ -10,85 +10,100 @@ class AutomaticScrollCard extends StatefulWidget {
 }
 
 class _AutomaticScrollCardState extends State<AutomaticScrollCard> {
-  List<Widget> cards = [
-    Card(
-      child: Container(
-        width: double.infinity,
-        height: 200,
-        color: Colors.white,
-        child: Image.asset("lib/images/cardI.jpg"),
-      ),
-    ),
-    Card(
-      child: Container(
-        width: double.infinity,
-        height: 200,
-        child: Image.asset("lib/images/cardII.jpg"),
-      ),
-    ),
-    Card(
-      child: Container(
-        width: double.infinity,
-        height: 200,
-        child: Image.asset("lib/images/cardIII.jpg"),
-      ),
-    ),
-    Card(
-      child: Container(
-        width: double.infinity,
-        height: 200,
-        child: Image.asset("lib/images/cardIV.jpg"),
-      ),
-    ),
-    Card(
-      child: Container(
-        width: double.infinity,
-        height: 200,
-        child: Image.asset("lib/images/cardV.jpg"),
-      ),
-    ),
+
+  final List <String> images=[
+    "lib/images/cardV.jpg",
+    "lib/images/cardI.jpg",
+    "lib/images/cardII.jpg",
+    "lib/images/cardIII.jpg",
+    "lib/images/cardIV.jpg",
+
   ];
+  // List<Widget> cards = [
+  //   Card(
+  //     child: Container(
+  //       width: double.infinity,
+  //       height: 200,
+  //       color: Colors.white,
+  //       child: Image.asset("lib/images/cardI.jpg"),
+    //   ),
+    // ),
+    // Card(
+    //   child: Container(
+    //     width: double.infinity,
+    //     height: 200,
+    //     child: Image.asset("lib/images/cardII.jpg"),
+    //   ),
+    // ),
+    // Card(
+    //   child: Container(
+  //       width: double.infinity,
+  //       height: 200,
+  //       child: Image.asset("lib/images/cardIII.jpg"),
+  //     ),
+  //   ),
+  //   Card(
+  //     child: Container(
+  //       width: double.infinity,
+  //       height: 200,
+  //       child: Image.asset("lib/images/cardIV.jpg"),
+  //     ),
+  //   ),
+  //   Card(
+  //     child: Container(
+  //       width: double.infinity,
+  //       height: 200,
+  //       child: Image.asset("lib/images/cardV.jpg"),
+  //     ),
+  //   ),
+  // ];
 
   PageController controller = PageController();
 
   late Timer timer;
 
   @override
-  void initState() {
-    super.initState();
-    // Start the timer when the widget is initialized
-    timer = Timer.periodic(Duration(seconds: 2), (Timer t) {
-      if (controller.hasClients && controller.page != null) 
-      {
-        if (controller.page == cards.length - 1) {
-          controller.animateToPage(0,
-              duration: Duration(milliseconds: 500), curve: Curves.easeInOut);
-        } else {
-          controller.nextPage(
+  // void initState() {
+  //   super.initState();
+  //   // Start the timer when the widget is initialized
+  //   timer = Timer.periodic(Duration(seconds: 2), (Timer t) {
+  //     if (controller.hasClients && controller.page != null) 
+  //     {
+  //       if (controller.page == cards.length - 1) {
+  //         controller.animateToPage(0,
+  //             duration: Duration(milliseconds: 500), curve: Curves.easeInOut);
+  //       } else {
+  //         controller.nextPage(
           
-              duration: Duration(milliseconds: 500), curve: Curves.easeIn);
-         }
-      }
-    });
-  }
+  //             duration: Duration(milliseconds: 500), curve: Curves.easeIn);
+  //        }
+  //     }
+  //   });
+  // }
 
-  @override
-  void dispose() {
-    // Cancel the timer when the widget is disposed to prevent memory leaks
-    timer.cancel();
-    super.dispose();
-  }
+  // @override
+  // void dispose() {
+  //   // Cancel the timer when the widget is disposed to prevent memory leaks
+  //   timer.cancel();
+  //   super.dispose();
+  // }
 
   @override
   Widget build(BuildContext context) {
+    final double width =MediaQuery.of(context).size.width;
     return Container(
       
-      child: Expanded(
-        child: PageView(
-          controller: controller,
-          children: cards.toList(),
-        ),
-      ),
+      child: CarouselSlider(
+        items:images.map((item) => Image.asset(item,)).toList(),
+        options:CarouselOptions(
+          height:200.0,
+          ViewportFraction:1,
+          enlargeCenterPage:false,
+          autoPlay:true,
+          autoPlayInterval:const Duration(seconds: 3),
+          autoPlayAnimationDuration:const Duration(microseconds: 800),
+        )
+      )
     );
   }
 }
